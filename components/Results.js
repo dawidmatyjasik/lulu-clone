c1d91741ff98d83946e4ae70821759b5b2904fe5
Dawid Spisak <dawidspisak@gmail.com>
2021-11-24 21:24:23 +0100

    create results

---
+++ components/Results.js
@@ -0,0 +1,9 @@
+const Results = () => {
+  return (
+    <div>
+      <h1>results</h1>
+    </div>
+  );
+};
+
+export default Results;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
       <Header />
       <Nav />
+      <Results />
     </div>
   );
 }
